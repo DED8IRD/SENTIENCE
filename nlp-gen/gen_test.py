f3e65e033d4e87db8a7069a747dacb78a53809be
DED8IRD <ecwu@ucsc.edu>
2016-11-19 11:24:34 -0800

    first commit

---
+++ nlp-gen/gen_test.py
@@ -0,0 +1,50 @@
+import nltk, os, time, itertools, pickle
+from collections import defaultdict, Counter
+from generator import sentGenerator 
+
+def makeNgrams(filename, words, n):
+    """
+    Generate n-grams from corpus
+    Returns a dictionary of k-grams (from 2 to nth degree)
+    """
+    start_time = time.time()
+    ngrams = dict()
+    itergrams = dict()
+
+    for k in range(2,n+1):
+        itergrams[k] = list(nltk.ngrams(words, k))
+
+    for k, grams in itergrams.items():
+        kgrams = defaultdict(Counter)
+        for gram in grams:                
+            kgram = list(gram)
+            key = ' '.join(kgram[:k-1])
+            kgrams[key].update({kgram[-1]})
+        ngrams[k] = kgrams
+        print ('finish gen ', k, 'grams at ', time.time()-start_time)
+    pickle.dump(ngrams, open(filename, 'wb'))
+
+if __name__ == '__main__':
+
+    # dataset_dict = {}
+
+    # # iterate through all the files in the current directory
+    # for filename in os.listdir("."):
+    #     if filename.endswith('.txt'):
+    #         dataset_dict[filename] = open(filename, 'rU', encoding='utf-8', errors='ignore').read()
+
+    # for filename, infile in dataset_dict.items():
+    #     filename = filename + ".pickle"
+    #     words = nltk.word_tokenize(infile)
+    #     ngrams = makeNgrams(filename, words, 12)
+        
+
+    start_time = time.time()
+    ngrams = pickle.load(open('post_compilation.txt.pickle', 'rb'))
+    ngrams_out = open('ngrams', 'w')
+    gen_out = open('generated.out', 'w')
+    print(ngrams, file=ngrams_out)
+    gen = sentGenerator(ngrams)
+    print(gen(), file=gen_out)
+    print ('finish gen at ', time.time()-start_time)
+
---
+++ nlp-gen/generator.py
@@ -0,0 +1,110 @@
+''' 
+generator.py
+
+Generates sentences through predicting the next possible word in sentence
+based on the context of its preceding words. The next word selected is based
+on conditional frequency distributions of n-grams found in the text, with 
+(1<k<n) fallback k-grams.
+'''
+
+import nltk, random, itertools, bisect, time
+from collections import defaultdict, Counter
+
+TERMINAL_PUNCT = ['.', '!', '?', '\n\n']
+SPLIT_PUNCT = [',', ';']
+CLAUSE_TERMINALS = TERMINAL_PUNCT + SPLIT_PUNCT
+STOPWORDS = nltk.corpus.stopwords.words('english')
+n = 5
+
+class sentGenerator(object):
+
+    def __init__(self, ngrams, *args, **kwargs):
+        if isinstance(ngrams, dict):
+            self.ngrams = ngrams
+    
+    def __call__(self, repetition=10, seed='The'): 
+        # if not seed:
+        #     print(self.ngrams[2])
+        #     seed = random.choice(self.ngrams[2].keys())
+        sentlen = 10          
+        return self.__generateSentences(self.ngrams, n, sentlen,
+                                         repetition, seed)
+
+    def __makeNgrams(self, n):
+        """
+        Generate n-grams from corpus
+        Returns a dictionary of k-grams (from 2 to nth degree)
+        """
+        start_time = time.time()
+        ngrams = dict()
+        itergrams = dict()
+
+        for k in range(2,n+1):
+            itergrams[k] = list(nltk.ngrams(self.words, k))
+
+        for k, grams in itergrams.items():
+            kgrams = defaultdict(Counter)
+            for gram in grams:                
+                kgram = list(gram)
+                key = ' '.join(kgram[:k-1])
+                kgrams[key].update({kgram[-1]})
+            ngrams[k] = kgrams
+            print ('finish gen ', k, 'grams at ', time.time()-start_time)
+        return ngrams
+
+    # Generates sentences using a n degree Markov model
+    def __markovGen(self, ngrams, n, length, start):
+        """
+        Param:
+            ngrams : dict
+                Dictionary of n-grams
+                Each key is a word in the corpus
+                Each value are possible n-grams with the starting word
+
+            length : int
+                Sentence length
+
+            start : string
+                Starting word for the generated sentence
+                Randomly chosen if not specified
+        """
+        sent = [start]
+        prev = start
+        for i in range(length):
+            k = len(sent)+1 if len(sent)+1 < n else n
+            while not ngrams[k][prev] and k > 2:
+                k -= 1
+            prev = ' '.join(sent[-k+1:])
+
+            weightedChoices = [(candidate, weight) for (candidate, weight) 
+                                in ngrams[k][prev].most_common(10)]
+            
+            # Choose next candidate word based on a cumulative weight distribution
+            choices, weights = zip(*weightedChoices)
+            cumdist = list(itertools.accumulate(weights))
+            choice = random.random() * cumdist[-1]
+            next = choices[bisect.bisect(cumdist, choice)]
+            sent.append(next)
+            # if sent[-1] in CLAUSE_TERMINALS:
+            #     sent.append('')
+            #     break
+            
+        ret = ' '.join(sent)
+        cleanPunct = CLAUSE_TERMINALS + ['\'','n\'t',':',')']
+        for punct in cleanPunct:
+            ret = ret.replace(' %s' % punct, punct)
+        ret = ret.replace('%s ' % '(', '(')
+        return ret
+
+    def __generateSentences(self, ngrams, n, length, repetition, seed):
+        """
+        Generates numSent number of sentences.
+        """
+        randInt = random.randint(1, repetition)
+        sent = ''
+        for i in range(randInt):
+            sent += self.__markovGen(self.ngrams, n, length, seed)
+            sent += ' '
+        return sent
+
+
