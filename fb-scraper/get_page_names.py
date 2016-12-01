1ceb53ee16835ce8c52970500dd35b2fb2ae4e9e
DED8IRD <ecwu@ucsc.edu>
2016-12-01 02:15:49 -0800

    Python 2.7 compatible

---
+++ fb-scraper/get_page_names.py
@@ -0,0 +1,15 @@
+"""
+get_page_names.py
+
+Gets page ids from csvs.
+"""    
+
+import os
+from collections import defaultdict
+
+if __name__ == '__main__':
+    output = open("page_names", "w")
+    # iterate through all the files in the current directory
+    for filename in os.listdir('.'):
+        if filename.endswith('.csv'):
+            output.write('\n'.join(str(filename.split('_')[0])))
---
+++ fb-scraper/get_posts.py
@@ -0,0 +1,55 @@
+
+#!/usr/bin/env python
+'''
+ get_posts.py
+ Gets post data from csv files in this directory.
+ Python 2.7 compatible.
+'''
+
+from __future__ import unicode_literals
+import zipfile, argparse, os, csv, re, io
+from collections import defaultdict
+
+###############################################################################
+## Utility Functions ##########################################################
+###############################################################################
+
+def getAttrLists(attr):
+    """
+    Processes all csv files within the current directory and returns the columns
+    associated with the attribute attr.
+    Assumption: this python program is in the same directory as the training files.
+    """
+    columns = defaultdict(list) # each value in each column is appended to a list
+
+    # iterate through all the files in the current directory
+    for filename in os.listdir('.'):
+        if filename.endswith('.csv'):
+            with open(filename, 'rb') as f:
+                reader = csv.DictReader(f)   # read rows into as dict (col:val)
+                for row in reader:
+                    for (k,v) in row.items(): 
+                        if k == attr:
+                            v = v.decode('utf-8', errors='replace')
+                            columns[k].append(v)
+
+    return columns[attr]
+
+
+###############################################################################
+## Program Entry Point ########################################################
+###############################################################################
+if __name__ == '__main__':
+    with io.open("post_compilation", "w", encoding='utf-8') as output:
+            posts = getAttrLists('status_message')
+            for post in posts:
+                if len(post) > 0:
+                    # To deal with page quote prevalence
+                    if not re.match(r'^.* .* .*:', post) \
+                        and not re.search(r'The Meme Games', post) \
+                        and not re.search(r'Surface Reality', post) \
+                        and not re.search(r'The Epic Department', post) \
+                        and not re.search(r'The Content Zone', post) \
+                        and not re.search(r'Kevin 3', post) \
+                        and not re.search(r'The Needle Drop', post):
+                            output.write(post + '\n\n')
