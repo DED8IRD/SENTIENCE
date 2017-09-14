71fef2766e1089d715ad864a5fe7edfba801db98
CarpeSwag <james@garbagnati.com>
2017-09-14 00:07:06 -0700

    Renamed the page and cleaned some garbage.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,12 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			msg: "hello"
 		};
 	}
 	
 	render() {
 		return (
 			<div>
-				<h1> TEST </h1>
 			</div>
 		);
 	}
