9a55a335cc247403e05813a68a34cd176f05407f
DED8IRD <ewu72829@gmail.com>
2018-04-11 02:38:35 -0700

    added .gitignore

--- django/SENTIENCE/SENTIENCE/settings.py
+++ django/SENTIENCE/SENTIENCE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(+seh5h&@-*97h38ofkafa0^isw-7sbcb#99o(r5e)poalh@=3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
